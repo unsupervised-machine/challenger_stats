8b9924b1581c50103321b0dd7cba5e1adc2f4200
unsupervised-machine <taran.s.lau@gmail.com>
2024-10-19 18:23:34 -0700

    - backend main app now works, start with command: uvicorn backend.app.main:app --reload - add logging to routes.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
-from app.routes import router as data_router
+from fastapi.middleware.cors import CORSMiddleware
+from backend.app.routes import router as data_router
 import uvicorn
+
+
+# to start app use following command from project directory (challenger_stats dir): uvicorn backend.app.main:app --reload
 
 app = FastAPI()
 
+
+# Enable CORS for your frontend application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace with your frontend URL in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Register API routes
 app.include_router(data_router)
+
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the Player Stats API!"}
+
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,16 @@
 from fastapi import APIRouter
 from backend.app.db.db_queries import query_player_stats_all, query_player_stats_by_id, compile_match_detail_from_puuid
 import asyncio
+import logging
+
+
+# Configure logging
+logging.basicConfig(
+    filename="routes.log",  # Log file name
+    level=logging.INFO,  # Logging level
+    format="%(asctime)s - %(levelname)s - %(message)s",  # Log message format
+    datefmt="%Y-%m-%d %H:%M:%S",  # Date format for timestamps
+)
 
 router = APIRouter()
 
@@ -9,23 +19,25 @@ async def get_player_stats_all_db():
     data = await query_player_stats_all()  # Fetch data from MongoDB
     return data
 
-@router.get("/api/player-stats/{puuid}")
+@router.get("/api/player-stats/{player_puuid}")
 async def get_player_stats_db(player_puuid: str):
     data = await query_player_stats_by_id(player_puuid=player_puuid)  # Fetch data from MongoDB
     return data
 
-@router.get("/api/player-match-history{puuid}")
+@router.get("/api/player-match-history{player_puuid}")
 async def get_player_match_history_db(player_puuid: str):
+    logging.info(f"START ROUTE: /api/player-match-history{player_puuid}")
     data = await compile_match_detail_from_puuid(player_puuid=player_puuid)
+    logging.info(f"returned data sample: {data[0:1]}")
+    logging.info(f"returned data length: {len(data)}")
+    logging.info(f"END ROUTE: /api/player-match-history{player_puuid}")
+    return data
 
 
 
 # Running the async function in an event loop
 if __name__ == "__main__":
-    results = asyncio.run(get_player_stats_all_db())
-    # results = asyncio.run(get_player_stats_db(player_puuid="RzdoQhmL8pdy8Oj8MweL584sMtP6pdn5TwcXpeq-9OnGeOmUrFaWc8NECbYF_kG4C0aQg-PjSxCuyQ"))
-
-
-    sample = results[0:10]
-    print(f"sample, {sample}")
-    print(len(results))
+    sample_puuid = "RzdoQhmL8pdy8Oj8MweL584sMtP6pdn5TwcXpeq-9OnGeOmUrFaWc8NECbYF_kG4C0aQg-PjSxCuyQ"
+    # results = asyncio.run(get_player_stats_all_db())
+    # results = asyncio.run(get_player_stats_db(player_puuid=sample_puuid))
+    results = asyncio.run(get_player_match_history_db(player_puuid=sample_puuid))
